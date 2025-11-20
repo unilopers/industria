3b70e7c46034fcc844b0db24212355d53de3851d
JuniorJose-git <joseronaldosantus@gmail.com>
2025-11-19 21:01:21 -0300

    ➕ adicionado dependências do springboot

---
+++ src/main/java/com/industria/cafeeira/CafeeiraApplication.java
@@ -0,0 +1,13 @@
+package com.industria.cafeeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeeiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeeiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/industria/cafeeira/CafeeiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.industria.cafeeira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeeiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
