2300f566f624fd8cfd9e16c5127a179b1e507881
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-22 03:01:13 -0300

    🔨 Adicionado contrutores da Entity Usuario

--- src/main/java/com/industria/cafeeira/model/entities/Usuario.java
+++ src/main/java/com/industria/cafeeira/model/entities/Usuario.java
@@ -1,11 +1,18 @@
 package com.industria.cafeeira.model.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Usuario")
 public class Usuario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne
+    @JoinColumn(name = "fk_funcionario", nullable = false, unique = true)
+    private Funcionario funcionario;
+    private String login;
+    private String senha;
 
 }
