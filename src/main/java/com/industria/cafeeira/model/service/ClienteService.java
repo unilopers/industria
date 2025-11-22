12d87c6d6cd2bec82db2a100906d4be0b2af6360
Isabely Cristina <isabelycristinacm@edu.unifil.br>
2025-11-22 16:49:45 -0300

    ✨ Adicionei um método de cadastrar Cliente no ClienteService

---
+++ src/main/java/com/industria/cafeeira/model/service/ClienteService.java
@@ -0,0 +1,32 @@
+package com.industria.cafeeira.model.service;
+
+import com.industria.cafeeira.model.entities.Cliente;
+import com.industria.cafeeira.model.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public Cliente cadastrarCliente(Cliente cliente){
+        if(clienteRepository.existsByCodigo(cliente.getCodigo())){
+            throw new RuntimeException("Já existe um Cliente com esse código");
+        } else if (clienteRepository.existsByCpf(cliente.getCpf())) {
+            throw new RuntimeException("Já existe um Cliente com esse Cpf");
+        } else if (clienteRepository.existsByCnpj(cliente.getCnpj())) {
+            throw new RuntimeException("Já existe um Cliente com esse Cnpj");
+        } else if (clienteRepository.existsByRazaoSocial(cliente.getRazaoSocial())) {
+            throw new RuntimeException("Já existe um Cliente com essa Razão Social");
+        }
+
+        try{
+            return clienteRepository.save(cliente);
+        }catch (DataIntegrityViolationException e){
+            throw new RuntimeException("Cliente já cadastrado");
+        }
+    }
+}
