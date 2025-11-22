38212677dca62a97076599c74943caee454848f9
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-22 02:37:53 -0300

    🔧 Configuração da FuncionarioService com @Service e @Autowired

--- src/main/java/com/industria/cafeeira/model/service/FuncionarioService.java
+++ src/main/java/com/industria/cafeeira/model/service/FuncionarioService.java
@@ -1,4 +1,14 @@
 package com.industria.cafeeira.model.service;
 
+import com.industria.cafeeira.model.repository.FuncionarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FuncionarioService {
+
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+
+
 }
