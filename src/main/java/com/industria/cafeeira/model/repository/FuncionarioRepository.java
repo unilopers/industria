c0d8c2c20c9cd13a34753e67364dfcaec955aea0
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-22 02:33:36 -0300

    🔨 Adicionado @Repository ao FuncionarioRepository

--- src/main/java/com/industria/cafeeira/model/repository/FuncionarioRepository.java
+++ src/main/java/com/industria/cafeeira/model/repository/FuncionarioRepository.java
@@ -2,7 +2,9 @@ package com.industria.cafeeira.model.repository;
 
 import com.industria.cafeeira.model.entities.Funcionario;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Long> {
 
 }
