ef9b0b34bff74fcca3faf7f240a256e6799e2365
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-22 02:48:13 -0300

    ✨  Adiciona método novoFuncionario na Service

--- src/main/java/com/industria/cafeeira/model/service/FuncionarioService.java
+++ src/main/java/com/industria/cafeeira/model/service/FuncionarioService.java
@@ -1,5 +1,6 @@
 package com.industria.cafeeira.model.service;
 
+import com.industria.cafeeira.model.entities.Funcionario;
 import com.industria.cafeeira.model.repository.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,5 +11,12 @@ public class FuncionarioService {
     @Autowired
     private FuncionarioRepository funcionarioRepository;
 
+    public boolean novoFuncionario(Funcionario funcionario) throws Exception{
+        if (funcionario.getNome_completo() == null || funcionario.getNome_completo().isEmpty()){
+            throw new Exception("O nome do Funcionário precisa ser preenchido corretamente");
+        }
 
+        funcionarioRepository.save(funcionario);
+        return true;
+    }
 }
