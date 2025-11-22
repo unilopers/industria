886a985ef1a60ff6054ef71257a64ef61ea8432f
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-22 14:56:14 -0300

    ✨  Adicionado a injeção da dependência do UsuarioRepository e o metodo novoUsuario na UsuarioService

--- src/main/java/com/industria/cafeeira/model/service/UsuarioService.java
+++ src/main/java/com/industria/cafeeira/model/service/UsuarioService.java
@@ -1,4 +1,23 @@
 package com.industria.cafeeira.model.service;
 
+import com.industria.cafeeira.model.entities.Usuario;
+import com.industria.cafeeira.model.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public boolean novoUsuario(Usuario usuario) throws Exception{
+        if (usuario.getFuncionario() == null || usuario.getFuncionario().getNome_completo().isEmpty() || usuario.getFuncionario().getNome_completo() == null){
+            throw new IllegalArgumentException("O nome do Usuário precisa ser preenchido corretamente");
+        }
+        usuarioRepository.save(usuario);
+        return true;
+    }
+
+
 }
