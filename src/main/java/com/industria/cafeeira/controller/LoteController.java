42d02dd720d5421f88b9fae71aa8f5a4ba7323df
JorgeSkraba <jorgeskraba@edu.unifil.br>
2025-11-22 12:55:07 -0300

    :sparkles: Adição da classe LoteController para mapeamento do CRUD da entidade Lote

---
+++ src/main/java/com/industria/cafeeira/controller/LoteController.java
@@ -0,0 +1,44 @@
+package com.industria.cafeeira.controller;
+
+import com.industria.cafeeira.model.entities.Lote;
+import com.industria.cafeeira.model.repository.LoteRepository;
+import com.industria.cafeeira.model.service.LoteService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/lotes")
+public class LoteController {
+
+    private final LoteService service;
+
+    public LoteController(LoteService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public Lote create(@RequestBody Lote lote){
+        return service.save(lote);
+    }
+
+    @GetMapping
+    public List<Lote> findAll(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Lote findById(@PathVariable Long id){
+        return service.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Lote update(@PathVariable Long id, @RequestBody Lote lote){
+        return service.update(id, lote);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id){
+        service.delete(id);
+    }
+}
