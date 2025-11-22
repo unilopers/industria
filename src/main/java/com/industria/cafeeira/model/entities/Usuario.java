d7835cc487b25ddb6f4945b25c7a3cf04a31043e
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-22 02:55:23 -0300

    🔧 Adicionadas anotações @Entity e @Table na entity Usuario

--- src/main/java/com/industria/cafeeira/model/entities/Usuario.java
+++ src/main/java/com/industria/cafeeira/model/entities/Usuario.java
@@ -1,4 +1,11 @@
 package com.industria.cafeeira.model.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Usuario")
 public class Usuario {
+
+
 }
