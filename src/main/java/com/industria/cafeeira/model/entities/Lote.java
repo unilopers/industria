f90c1395ffd6af183d3baf7758c6baea040d526c
JorgeSkraba <jorgeskraba@edu.unifil.br>
2025-11-22 12:48:34 -0300

    :sparkles: Adição da entidade Lote

---
+++ src/main/java/com/industria/cafeeira/model/entities/Lote.java
@@ -0,0 +1,116 @@
+package com.industria.cafeeira.model.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "lotes")
+public class Lote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String codigo;
+
+    @Column(nullable = true)
+    private long fk_fornecedor;
+
+    @Column(nullable = true)
+    private long fk_produto;
+
+    private String tipo;
+    private BigDecimal quantidade;
+    private Date data_vencimento;
+    private String descricao;
+
+    public Lote(){
+
+    }
+
+    public Lote(Long id,
+                String codigo,
+                long fk_fornecedor,
+                long fk_produto,
+                String tipo,
+                BigDecimal quantidade,
+                Date data_vencimento,
+                String descricao){
+
+        this.id = id;
+        this.codigo = codigo;
+        this.fk_fornecedor = fk_fornecedor;
+        this.fk_produto = fk_produto;
+        this.tipo = tipo;
+        this.quantidade = quantidade;
+        this.data_vencimento = data_vencimento;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public long getFk_fornecedor() {
+        return fk_fornecedor;
+    }
+
+    public void setFk_fornecedor(long fk_fornecedor) {
+        this.fk_fornecedor = fk_fornecedor;
+    }
+
+    public long getFk_produto() {
+        return fk_produto;
+    }
+
+    public void setFk_produto(long fk_produto) {
+        this.fk_produto = fk_produto;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public BigDecimal getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(BigDecimal quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Date getData_vencimento() {
+        return data_vencimento;
+    }
+
+    public void setData_vencimento(Date data_vencimento) {
+        this.data_vencimento = data_vencimento;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
