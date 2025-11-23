6a7f668150511f44b48f7a4117b62d18b2aa86b2
Isabely Cristina <isabelycristinacm@edu.unifil.br>
2025-11-23 12:39:11 -0300

    ✨ Criando uma nova classe DTO para transportar dados

---
+++ src/main/java/com/industria/cafeeira/model/DTO/PedidoDto.java
@@ -0,0 +1,17 @@
+package com.industria.cafeeira.model.DTO;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class PedidoDto {
+    @NotNull(message = "O código do pedido é obrigatório")
+    private String codigoPedido;
+    @NotNull(message = "O cliente é obrigatório")
+    private Long idCliente;
+}
