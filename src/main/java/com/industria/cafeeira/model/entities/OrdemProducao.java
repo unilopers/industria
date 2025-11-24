c9ac21a35dc1c4ee832de4d55348ac75b4afeaa5
JuniorJose-git <joseronaldosantus@gmail.com>
2025-11-23 22:13:22 -0300

    ✨ adicionado OrdemProducaoRepository

--- src/main/java/com/industria/cafeeira/model/entities/OrdemProducao.java
+++ src/main/java/com/industria/cafeeira/model/entities/OrdemProducao.java
@@ -1,4 +1,28 @@
 package com.industria.cafeeira.model.entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "ordens_producao")
 public class OrdemProducao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(unique = true)
+    private String codigo;
+
+    @ManyToOne
+    @JoinColumn(name="lote_id")
+    private Lote lote;
+
+    @ManyToOne
+    @JoinColumn(name="etiqueta_id")
+    private Etiqueta etiqueta;
+
+    @ManyToOne
+    @JoinColumn(name="pedido_id")
+    private Pedido pedido;
 }
