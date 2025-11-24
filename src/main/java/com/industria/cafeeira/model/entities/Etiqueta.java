5eb2c186a4cb2c8f0efefc9243f5591b4ae413d7
JuniorJose-git <joseronaldosantus@gmail.com>
2025-11-23 22:03:35 -0300

    ✨ adicionado etiquetaController

--- src/main/java/com/industria/cafeeira/model/entities/Etiqueta.java
+++ src/main/java/com/industria/cafeeira/model/entities/Etiqueta.java
@@ -1,10 +1,9 @@
 package com.industria.cafeeira.model.entities;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
+@Table(name = "etiquetas")
 public class Etiqueta {
 
     @Id
@@ -12,6 +11,22 @@ public class Etiqueta {
     @Column(name = "id")
     private Long id;
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true)
     private String codigo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
 }
