ffff97c3d12d88aad9fb440ee9dabf8beb1e792c
JuniorJose-git <joseronaldosantus@gmail.com>
2025-11-23 21:19:19 -0300

    🩹 Ajuste dos conflitos de merge

--- src/main/java/com/industria/cafeeira/model/repository/FuncionarioRepository.java
+++ src/main/java/com/industria/cafeeira/model/repository/FuncionarioRepository.java
@@ -1,11 +1,13 @@
 package com.industria.cafeeira.model.repository;
 
+import com.industria.cafeeira.model.entities.Cliente;
 import com.industria.cafeeira.model.entities.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FuncionarioRepository extends CrudRepository<Funcionario, Long> {
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Long> {
 
 
 }
