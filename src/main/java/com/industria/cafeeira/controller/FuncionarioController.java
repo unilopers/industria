36616fe0135acb07d6efd2e064bc38c94058723c
Ysadora Sistini <sistiniysadora@gmail.com>
2025-11-25 20:12:42 -0300

    ✨ Adicionado listagem e leitura em Funcionado Controller

--- src/main/java/com/industria/cafeeira/controller/FuncionarioController.java
+++ src/main/java/com/industria/cafeeira/controller/FuncionarioController.java
@@ -1,4 +1,37 @@
 package com.industria.cafeeira.controller;
 
+
+import com.industria.cafeeira.model.service.FuncionarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/funcionario")
 public class FuncionarioController {
+
+    @Autowired
+    private FuncionarioService funcionarioService;
+
+    @GetMapping
+    public ResponseEntity<List<?>> list(){
+        return ResponseEntity.ok(funcionarioService.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> read(Long id){
+        try {
+            return ResponseEntity.ok(funcionarioService.findById(id));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+
 }
